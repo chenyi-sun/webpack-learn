f4e94db8d1b6a6199702518c45c8fbfe8a6af051
/ </>
2017-05-26 08:52:53 +0800

    change

--- learn5/app/main.js
+++ learn5/app/main.js
@@ -1,6 +1,2 @@
-// var greeter = require('./Greeter.js');
-// import './main.css';
 import './css/mains.less';
-// document.getElementById('root').appendChild(greeter());
-// let sjj = "sjsjsjsjss";
-// console.log(sjj);
+
