99c3ace253d0a5399e9410ff70a75b68baab44b6
/ </>
2017-06-13 17:17:22 +0800

    improve

--- learn-vue/app/script/index.js
+++ learn-vue/app/script/index.js
@@ -2,15 +2,27 @@ require('./../css/main.scss');
 require('./all.js');
 require('./img');
 import Vue from 'vue';
+import axios from 'axios'
 
 function change(paremary){
     this.name = "ss";
     this.alert = paremary;
 }
-
+axios.get('http://apiqinbayou.ztxywy.net/app/service/category/index?openid=ocbOUs4UnYI7uQ4TEOG8Sr3MdsQc')
+.then(function(item){
+    console.log(item.data);
+    var ss = item.data;
+    var app = new Vue({
+        el: '#app',
+        data: {
+            message: item.data
+        }
+    })
+});
 var ss = {
     ss:'sss'
 }
 const longChance = ['sss', 'ksksksk'];
 let alls = longChance.map((item) =>  new change(item));
 console.log( Object.assign(...alls));
+
