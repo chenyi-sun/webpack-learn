6472b2f9c841606db40855b9ff22a6775fe04bb9
/ </>
2018-06-20 20:23:20 +0800

    add improve wabpack

--- webpack-4/src/script/index.js
+++ webpack-4/src/script/index.js
@@ -1,9 +1,16 @@
 require('./../css/index.css');
 require('./../less/index.less');
 require('./../less/common.less');
+require("velocity-animate");
 let  arrs = [];
 const  arr1 = ['a', 'b'];
 const  arr2 = ['c'];
 const  arr3 = ['d', 'e'];
+
+document.getElementsByClassName("shoc")[0].velocity({
+    width:"300px",
+    opacity:0
+},"easeInOutSine")
+
 
 arrs = [...arr1, ...arr2, ...arr3];
