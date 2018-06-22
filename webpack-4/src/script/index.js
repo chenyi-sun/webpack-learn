f587529ac57ad7f50c48b93bf02ff07b914f4b74
/ </>
2018-06-22 14:34:45 +0800

    improve md

--- webpack-4/src/script/index.js
+++ webpack-4/src/script/index.js
@@ -2,6 +2,8 @@ require('./../css/index.css');
 require('./../less/index.less');
 require('./../less/common.less');
 require("velocity-animate");
+import mdin from "./../md/in.js";
+import mdout from "./../md/out.js";
 let  arrs = [];
 const  arr1 = ['a', 'b'];
 const  arr2 = ['c'];
@@ -11,5 +13,12 @@ document.getElementsByClassName("shoc")[0].velocity({
     width:"300px",
     opacity:0
 },"easeInOutSine")
+var value = {
+    link1: "link1.ask.name.vue",
+    link2: "link2.ask.name.mark.mask",
+    link3: "link3.ask.vue.miss"
+}
+// console.log(mdin(value));
+console.log(mdout(mdin(value)));
 
 arrs = [...arr1, ...arr2, ...arr3];
