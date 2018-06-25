75cdc9d4d8a860537abd349601710f68e8b05d5a
/ </>
2018-06-25 13:21:18 +0800

    improve vue components

--- webpack-4 - vue2/src/script/index.js
+++ webpack-4 - vue2/src/script/index.js
@@ -4,28 +4,45 @@ require('./../less/common.less');
 import mdin from "./../md/in.js";
 import mdout from "./../md/out.js";
 import Vue from 'vue';
+import VueRouter from 'vue-router';
+import inde from './../components/index.vue';
 let  arrs = [];
 const  arr1 = ['a', 'b'];
 const  arr2 = ['c'];
 const  arr3 = ['d', 'e'];
-console.log(Vue);
-// document.getElementsByClassName("shoc")[0].velocity({
-//     width:"300px",
-//     opacity:0
-// },"easeInOutSine")
+
+Vue.use(VueRouter);
+
 var value = {
     link1: "link1.ask.name.vue",
     link2: "link2.ask.name.mark.mask",
     link3: "link3.ask.vue.miss"
-}
+};
+
+ const Foo = {
+    template: '<div>foo</div>'
+ };
+ var routes = [
+     {
+        path: '/foo', component: Foo
+     }
+ ];
+ const router = new VueRouter({
+     routes
+ });
 
  var app = new Vue({
         // router,
         el: '#app',
         data: {
             name: "ssss",
+        },
+        router,
+        components: {
+            inde
         }
  });
+
 // console.log(mdin(value));
 console.log(mdout(mdin(value)));
 
