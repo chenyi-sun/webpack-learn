bd1652d1ce2399b290fc0b997e48b14dff6e9bbd
/ </>
2018-06-26 14:47:31 +0800

    improve react-envirment

---
+++ webpack-4 - react/src/script/index.js
@@ -0,0 +1,19 @@
+require('./../css/index.css');
+require('./../less/index.less');
+require('./../less/common.less');
+import React from 'react';
+import {render} from 'react-dom';
+
+let  arrs = [];
+const  arr1 = ['a', 'b'];
+const  arr2 = ['c'];
+const  arr3 = ['d', 'e'];
+class shoppingList extends React.Component {
+    render() {
+        return (
+            <div>
+                <li>sssdddD</li>
+            </div>
+        );
+    }
+}
