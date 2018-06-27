af797c4bff6e27f6912e5e7a88bb543cfb8c25a4
/ </>
2018-06-27 16:17:37 +0800

    improve react dom

--- webpack-4 - react/src/script/index.js
+++ webpack-4 - react/src/script/index.js
@@ -2,18 +2,50 @@ require('./../css/index.css');
 require('./../less/index.less');
 require('./../less/common.less');
 import React from 'react';
-import {render} from 'react-dom';
+import ReactDom, {render} from 'react-dom';
 
 let  arrs = [];
 const  arr1 = ['a', 'b'];
 const  arr2 = ['c'];
 const  arr3 = ['d', 'e'];
-class shoppingList extends React.Component {
-    render() {
+
+class Clock extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {date: new Date()};
+    }
+
+    componentDidMount(){ //组件被输出到DOM之后运行
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount(){ //当组件被注销的时候执行
+        clearInterval(this.timerID);
+    }
+
+    tick(){
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render(){
         return (
             <div>
-                <li>sssdddD</li>
+                <h1>Hello , world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}</h2>
             </div>
         );
     }
 }
+
+ReactDom.render(
+    <Clock/>,
+    document.getElementById('root')
+);
+
+
+
