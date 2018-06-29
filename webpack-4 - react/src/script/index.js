cc62693a701fbca67e9827c6771a16a0ebaf951c
/ </>
2018-06-29 10:53:40 +0800

    add react set

--- webpack-4 - react/src/script/index.js
+++ webpack-4 - react/src/script/index.js
@@ -2,81 +2,49 @@ require('./../css/index.css');
 require('./../less/index.less');
 require('./../less/common.less');
 import React from 'react';
-import ReactDom, {render} from 'react-dom';
+import ReactDom from 'react-dom';
 import { VelocityComponent } from 'velocity-react';
+import { createStore } from 'redux';
 
-const VelocityLetter = ({ letter }) => (
-  <VelocityComponent
-    runOnMount
-    animation={{ opacity: 1, marginTop: 0 }}
-    duration={500}
-  >
-    <p style={styles.letter}>{letter}</p>
-  </VelocityComponent>
-)
-
-
-class App extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            letters: [],
-        }
-        this.onChange = this.onChange.bind(this);
+const reducer = (state = 2, action) => {
+    if(action.type){
+        switch (action.type){
+            case 'INCREMENT': return action.text;
+            case 'DECREMENT': return state - 1;
+            default: return state;
+        } 
     }
-  onChange (e) {
-    const letters = e.target.value.split('');
-    const arr = []
-    letters.forEach((l, i) => {
-      arr.push(<VelocityLetter letter={l} key={i}/>)
-    })
-    this.setState(() => ({ letters: arr }))
-  }
+    else if(action){
 
-  render() {
-    return (
-      <div className="App">
-        <div className="container">
-          <input onChange={this.onChange} style={styles.input} />
-          <div style={styles.letters}>
-            {
-              this.state.letters
-            }
-          </div>
-        </div>
-      </div>
-    );
-  }
+    } 
 }
-
-const styles = {
-  input: {
-    height: 40,
-    backgroundColor: '#ddd',
-    width: 200,
-    border: 'none',
-    outline: 'none',
-    marginBottom: 20,
-    fontSize: 22,
-    padding: 8,
-  },
-  letters: {
-    display: 'flex',
-    height: 140,
-  },
-  letter: {
-    opacity: 0,
-    marginTop: 100,
-    fontSize: 22,
-    whiteSpace: 'pre',
-  }
-}
+const store = createStore(reducer);
 
 
-ReactDom.render(
-    <App/>,
-    document.getElementById('root')
+const Counter = ({value, onIncre}) => (
+    <div>
+        <div>{value}</div>
+        <button onClick = {onIncre}>+</button>
+        <button >-</button>
+    </div>
 );
+
+
+const name = "sss";
+const render = () => {
+    ReactDom.render(
+        <Counter 
+            value = {store.getState()}
+            onIncre = {() => store.dispatch({type: 'INCREMENT', text: 'ASK'})}
+        />,
+        document.getElementById("root")
+    );
+}
+render();
+store.subscribe(render);
+
+
+
 
 
 
